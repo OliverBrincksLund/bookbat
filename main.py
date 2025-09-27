353350e3f4d849e23766d76642d06ddf0495ba34
Oliver B. Lund <152693629+OliverBrincksLund@users.noreply.github.com>
2025-09-27 14:38:34 +0200

    main.py updates.

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath, encoding='utf-8') as f:
+       file_contents = f.read()
+    return file_contents
 
+
+def main():
+    text = get_book_text("C:/Github_repositories/bookbot/books/frankenstein.txt") 
+    print(text)
+
+if __name__ == "__main__":
+    main()
